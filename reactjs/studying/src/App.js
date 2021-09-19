57b5a10eb49071a10cff8af1f7ce725a2ef3e45a
mrdatngo <mrdatngo@gmail.com>
2021-09-19 21:40:24 +0700

    build project base CRA

---
+++ reactjs/studying/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './App.css';
+
+// function App() {
+//   return (
+//     <div>
+//       Hello ReactJS
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello ReactJS
+      </div>
+    )
+  }
+}
+
+export default App;
