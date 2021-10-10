b92d995bc4279a5e4c6cef6c8a7f72e219548efd
mrdatngo <mrdatngo@gmail.com>
2021-10-10 22:00:16 +0700

    add cors for admin api

--- reactjs/admin-api/server.js
+++ reactjs/admin-api/server.js
@@ -1,19 +1,35 @@
 const jsonServer = require('json-server')
 const jwt = require('jsonwebtoken');
+const cors = require('cors');
 
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const middlewares = jsonServer.defaults({ noCors: true })
+
+server.use(
+    cors({
+        origin: true,
+        credentials: true,
+        preflightContinue: false,
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    })
+);
+server.options('*', cors());
 
 server.use(middlewares)
-
 server.use(jsonServer.bodyParser)
+
+// server.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+//     res.header('Access-Control-Allow-Headers', '*')
+//     next()
+// })
 
 // Add custom routes before JSON Server router
 server.post('/auth/login', (req, res) => {
     const { username, password } = req.body
     if (username === "admin" && password === "123456") {
-        let token = jwt.sign({ username, roles: ["read_users"] }, 'this_is_a_private_key');
+        let token = jwt.sign({ username, roles: ["read_users"] }, 'this_is_a_private_key',  { expiresIn: 8 * 60 * 60 });
         res.jsonp({
             success: true,
             username: username,
